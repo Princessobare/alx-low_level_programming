d817590681467c6cc8ce1f47ba98d4ea1fff543e
Obare <obaredafiaghor@gmail.com>
2023-04-20 18:28:13 -0700

    puts command

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - serves as the  entry point for the program
+ * Return: return zero if no error, else return non-zero number
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
