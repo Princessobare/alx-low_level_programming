b6c45021e7e39aad9b3fca0ce7e6b4b96c6b8a89
Obare <obaredafiaghor@gmail.com>
2023-04-20 18:42:26 -0700

    printf in c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - serves as the  entry point for the program
+ * Return: return zero if no error, else return non-zero number
+ */
+
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,");
+        return (0);
+}
