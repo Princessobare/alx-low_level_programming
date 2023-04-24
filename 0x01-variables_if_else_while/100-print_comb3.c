865efcb80df902418d1c3cb08a94b938c7baab93
Obare <obaredafiaghor@gmail.com>
2023-04-24 14:27:14 -0700

    print double digits combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *description - print all possible combinations of double digit numbers
+ *Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 56; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
